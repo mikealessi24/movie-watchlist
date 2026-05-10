3ae348384eba57bd38b3081c529be039595798a1
Michael Alessi <60523720+mikealessi24@users.noreply.github.com>
2026-05-10 13:09:31 -0400

    fix toast messages

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { queryClient } from "@/lib/queryClient";
+import { Toaster } from "@/components/ui/sonner";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
@@ -15,6 +16,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
           enableSystem
           disableTransitionOnChange
         >
+          <Toaster />
           {children}
         </ThemeProvider>
       </QueryClientProvider>
